8c929e2f7186822b687599a6b8087728b4bfcfe6
Pratik Panchal <m3pratik@gmail.com>
2022-08-05 15:02:56 +0530

    ad-hoc commands with config

--- creating-temp-ec2-tf/provider.tf
+++ creating-temp-ec2-tf/provider.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "4.18.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.18.0"
+    }
+  }
+}
 
 provider "aws" {
   region = "ap-south-1"
--- creating-temp-ec2-tf/variables.tf
+++ creating-temp-ec2-tf/variables.tf
@@ -1,3 +1,11 @@
 variable "public_key" {
   default = ""
 }
+variable "access_key" {
+    type        = string
+    description = "This is an example input variable using env variables."
+}
+variable "secret_key" {
+    type        = string
+    description = "This is an example input variable using env variables."
+}
