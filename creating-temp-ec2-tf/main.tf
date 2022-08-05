74e607c89e7d13570e1de2abf1483c744854221c
Pratik Panchal <m3pratik@gmail.com>
2022-08-05 15:43:08 +0530

    updated records

--- creating-temp-ec2-tf/main.tf
+++ creating-temp-ec2-tf/main.tf
@@ -50,6 +50,14 @@ resource "aws_security_group" "allow_ssh" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   egress {
     from_port        = 0
     to_port          = 0
